e1e09033108de9a9068bd26d8328327a5607474d
miokato@me.com <nuntobunasan4696>
2018-04-29 17:17:52 +0900

    Release 0.0.6
    fix

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='MarkovGenerator',
-    version='0.0.5',
+    version='0.0.6',
     description='Text generator using the Markov model',
     author='Mio Kato',
     author_email='miokato07@gmail.com',
     url='https://github.com/miokato/MarkovGenerator',
     install_requires=['numpy', 'mecab-python3'],
-    package_dir=['MKGen'],
+    packages=['MKGen'],
 )
